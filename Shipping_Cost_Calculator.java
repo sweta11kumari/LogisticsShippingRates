fab90b89737729adb266db16d0d9a778fe476226
sweta11kumari <65407601+sweta11kumari@users.noreply.github.com>
2026-02-28 17:56:59 +0530

    Create Shipping_Cost_Calculator.java

---
+++ Shipping_Cost_Calculator.java
@@ -0,0 +1,16 @@
+# Calculate the total rate :
+import java.util.*;
+import java.scanner;
+
+System.out.println("Enter weight ");
+Scanner sc=new Scanner (System.in);
+double weight= sc.nextDouble();
+System.out.println("Enter rate ");
+Scanner sc=new Scanner(System.in);
+double rate= sc.nextDouble();
+
+#display the result
+
+double result = weight * rate;
+System.out.println("The result is " +result);
+
